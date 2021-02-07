cebbc89f3999e663d6464e991f3b702a91647631
Konetskaya <konetskaya.polina@gmail.com>
2021-02-07 23:25:49 +0100

    fixed url 'post_detail'

--- blog/myblog/blog/urls.py
+++ blog/myblog/blog/urls.py
@@ -4,7 +4,11 @@ from . import views
 app_name = "blog"
 
 urlpatterns = [
-    path('posts/', views.post_list, name='post_list'), 
-    path('<int:year>/<int:month>/<int:day>/<slug:post>/', views.post_detail, name='post_detail'),
-    path('<int:post_id>/share/', views.post_share, name='post_share')
+    path("posts/", views.post_list, name="post_list"),
+    path(
+        "<slug:slug>/<int:id>/",
+        views.post_detail,
+        name="post_detail",
+    ),
+    path("<int:post_id>/share/", views.post_share, name="post_share"),
 ]
