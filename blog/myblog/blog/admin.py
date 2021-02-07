98cce961991109f8dcc84895771f29cfe75f36bb
Konetskaya <konetskaya.polina@gmail.com>
2021-02-07 23:21:46 +0100

    added view_on_site

--- blog/myblog/blog/admin.py
+++ blog/myblog/blog/admin.py
@@ -2,28 +2,28 @@ from django.contrib import admin
 
 from .models import Post, Comment, Tag
 
+
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    list_display = ('title', 'slug', 'author', 'publish', 'status', 'get_tags')
-    list_filter = ('status', 'created', 'publish', 'author')
-    search_fields = ('title', 'body', 'tags')
-    prepopulated_fields = {'slug': ('title',)}
-    raw_id_fields = ('author',)
-    date_hierarchy = 'publish'
-    ordering = ['status', 'publish']
-    filter_horizontal = ('tags',)
+    list_display = ("title", "slug", "author", "publish", "status", "get_tags")
+    list_filter = ("status", "created", "publish", "author")
+    search_fields = ("title", "body", "tags")
+    prepopulated_fields = {"slug": ("title",)}
+    raw_id_fields = ("author",)
+    date_hierarchy = "publish"
+    ordering = ["status", "publish"]
+    filter_horizontal = ("tags",)
+    view_on_site = True
 
     def get_tags(self, obj):
         return ",".join([str(tag) for tag in obj.tags.all()])
 
-    
-
 
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
-    list_display = ('name', 'email', 'post', 'created', 'active')
-    list_filter = ('active', 'created', 'updated')
-    search_fields = ('name', 'email', 'body')
+    list_display = ("name", "email", "post", "created", "active")
+    list_filter = ("active", "created", "updated")
+    search_fields = ("name", "email", "body")
 
 
 # admin.site.register(Post, PostAdmin)
